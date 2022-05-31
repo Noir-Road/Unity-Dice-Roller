05ba068f970d7fc5ca0dff1b5aafb969d63c181e
Luigi-27 <85513457+noir-road@users.noreply.github.com>
2022-05-31 06:40:30 +0200

    Update PlayerRoll.cs
    Added a few commented lines for code.

--- Assets/Scripts/PlayerRoll.cs
+++ Assets/Scripts/PlayerRoll.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 public class PlayerRoll : MonoBehaviour
 {
-    [Header("Bomb Cooldown")] 
-    [SerializeField] float shakeForce;
-    [SerializeField] private float upDiceForce;
-    [SerializeField] Rigidbody rb;
+    [Header("Dice Cooldown")] 
+    [SerializeField] float shakeForce; // The force that will be applied to the phone in order to execute RollDice method.
+    [SerializeField] private float upDiceForce; // The force that will be applied to the dice to go up
+    [SerializeField] Rigidbody rb; // Objects Physics
     public enum Dice{ IDLE, ROLLING } 
     public Dice state;
     public Vector3 shaker;
@@ -19,14 +19,14 @@ public class PlayerRoll : MonoBehaviour
         shaker = rb.velocity;
         if(state == Dice.ROLLING) return;
         shaker = Input.acceleration;
-             
+             // Shake & if is greater than X, then it will launch the dice upwards.
         if (shaker.sqrMagnitude <= shakeForce) return;
         
         state = Dice.ROLLING;
         float dirX = RandomVelocity();
         float dirY = RandomVelocity();
         float dirZ = RandomVelocity();
-       // transform.position = new Vector3(0, .39f, 0);
+       // transform.position = new Vector3(0, .39f, 0); // Restart Dice from fixed position ?
         transform.rotation = Quaternion.identity; //Aligned for all axis
         rb.AddForce(transform.up * upDiceForce); // Raise the dice force
         rb.AddTorque(dirX,dirY,dirZ); // Apply torque in all axis
