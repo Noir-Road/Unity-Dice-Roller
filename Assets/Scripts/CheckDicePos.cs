40c5c8fa18c82dc2924fdadfda3d158a6bd16318
Luigi-27 <85513457+noir-road@users.noreply.github.com>
2022-05-31 06:45:16 +0200

    Update CheckDicePos.cs
    Added a few commented lines.

--- Assets/Scripts/CheckDicePos.cs
+++ Assets/Scripts/CheckDicePos.cs
@@ -1,7 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SubsystemsImplementation;
-
+// This script handles the position once the Dice land on ground {Cube}
 public class CheckDicePos : MonoBehaviour
 {
     Vector3 diceVelocity;
@@ -10,16 +10,16 @@ public class CheckDicePos : MonoBehaviour
 
     void FixedUpdate()
     {
-        diceVelocity = player.shaker;
+        diceVelocity = player.shaker; // Checking velocity from Dice, grabbing vars from PlayerRoll.cs
     }
 
     void OnTriggerStay(Collider dice)
     {
         // Check if the dice velocity in all axis is ZERO
      if (diceVelocity.x == 0f & diceVelocity.y == 0f && diceVelocity.z == 0f)
-     {
+     {  //If is Zero, then we can roll again and it will read wich face is facing ground so it can write the score on GUI.
          player.state = PlayerRoll.Dice.IDLE;
-         // Can roll again
+         
          score.text = dice.gameObject.name switch // Set score on UI
          {
              "Side 1" => "6",
