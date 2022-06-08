251fe3063bce01af338011145f94c7d982411036
Luigi-27 <85513457+noir-road@users.noreply.github.com>
2022-06-08 19:22:44 +0200

    Simplified code for checking velocity in all axis
    Check comments from code for more information.

--- Assets/Scripts/CheckDicePos.cs
+++ Assets/Scripts/CheckDicePos.cs
@@ -1,36 +1,24 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.SubsystemsImplementation;
-// This script handles the position once the Dice land on ground {Cube}
+
 public class CheckDicePos : MonoBehaviour
 {
-    Vector3 diceVelocity;
     [SerializeField] PlayerRoll player;
     [SerializeField] TextMeshProUGUI score;
-
-    void FixedUpdate()
-    {
-        diceVelocity = player.shaker; // Checking velocity from Dice, grabbing vars from PlayerRoll.cs
-    }
-
     void OnTriggerStay(Collider dice)
     {
-        // Check if the dice velocity in all axis is ZERO
-     if (diceVelocity.x == 0f & diceVelocity.y == 0f && diceVelocity.z == 0f)
-     {  //If is Zero, then we can roll again and it will read wich face is facing ground so it can write the score on GUI.
-         player.state = PlayerRoll.Dice.IDLE;
-         
-         score.text = dice.gameObject.name switch // Set score on UI
-         {
-             "Side 1" => "6",
-             "Side 2" => "5",
-             "Side 3" => "4",
-             "Side 4" => "3",
-             "Side 5" => "2",
-             "Side 6" => "1",
-             _ => score.text
-         };
-     }
-    
+        if (player.shaker != Vector3.zero) return; // Will check if the velocity of the dice is zero upon landing before updating the UI 
+        player.state = PlayerRoll.Dice.IDLE;        // & change the state back to idle, so it can roll again
+        score.text = dice.gameObject.name switch // Set score on UI
+        {
+            "Side 1" => "6",
+            "Side 2" => "5",
+            "Side 3" => "4",
+            "Side 4" => "3",
+            "Side 5" => "2",
+            "Side 6" => "1",
+            _ => score.text
+        };
     }
+}
 }
