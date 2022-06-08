35f06659134703a9b98ff891a2b6b64e3e1075c0
Luigi-27 <85513457+noir-road@users.noreply.github.com>
2022-06-08 19:21:46 +0200

    Added Space Bar, updates on velocity & spin Effect
    Check comments for more information.

--- Assets/Scripts/PlayerRoll.cs
+++ Assets/Scripts/PlayerRoll.cs
@@ -1,35 +1,53 @@
 using UnityEngine;
+
 public class PlayerRoll : MonoBehaviour
 {
-    [Header("Dice Cooldown")] 
-    [SerializeField] float shakeForce; // The force that will be applied to the phone in order to execute RollDice method.
-    [SerializeField] private float upDiceForce; // The force that will be applied to the dice to go up
-    [SerializeField] Rigidbody rb; // Objects Physics
-    public enum Dice{ IDLE, ROLLING } 
+    [Header("Dice Properties")] 
+    [SerializeField] float shakeForce; // The shake force that needs to be applied to Roll the Dice.
+
+    [SerializeField] float upDiceForce; // The force that the Dice will lift upon passing the Shake Force treshold variable.
+
+    [SerializeField] float airSpinEffect; // The rotation along the axis while the Dice is in the air. < The bigger the value, the rotation will be faster >
+
+    [SerializeField] Rigidbody rb;
+
+    public enum Dice
+    {
+        IDLE,
+        ROLLING
+    } // Either the Dice is Rolling or Idle
+
     public Dice state;
-    public Vector3 shaker;
-    
+    public Vector3 shaker; // Vector3 (x,y,z) values of the Dice
+
     void Update()
     {
-        RollDice();
+        shaker = rb.velocity;
+
+        // If the velocity of the dice is not zero, then is rolling
+        if (rb.velocity != Vector3.zero) state = Dice.ROLLING;
+        // If is rolling, then cannot roll again until the Dice stop
+        if (state == Dice.ROLLING) return;
+        shaker = Input.acceleration;
+
+        // If the shake force is greater than OR if we press the space bar - Then it will execute the below
+        if (shaker.sqrMagnitude >= shakeForce || Input.GetKeyDown("space"))
+        {
+            RollDice();
+        }
     }
 
     void RollDice()
     {
-        shaker = rb.velocity;
-        if(state == Dice.ROLLING) return;
-        shaker = Input.acceleration;
-             // Shake & if is greater than X, then it will launch the dice upwards.
-        if (shaker.sqrMagnitude <= shakeForce) return;
-        
-        state = Dice.ROLLING;
-        float dirX = RandomVelocity();
-        float dirY = RandomVelocity();
-        float dirZ = RandomVelocity();
-       // transform.position = new Vector3(0, .39f, 0); // Restart Dice from fixed position ?
+        float dirX = RandomVelocity(); //
+        float dirY = RandomVelocity(); //   Requesting Random Values from all Axis
+        float dirZ = RandomVelocity(); //
+        // transform.position = new Vector3(0, .39f, 0); // Manual position
         transform.rotation = Quaternion.identity; //Aligned for all axis
         rb.AddForce(transform.up * upDiceForce); // Raise the dice force
-        rb.AddTorque(dirX,dirY,dirZ); // Apply torque in all axis
+        rb.AddTorque(dirX, dirY, dirZ); // Apply torque in all axis
     }
-    float RandomVelocity() => Random.Range(0, 500); // Random Velocity for all axis
+
+    // If the airSpinEffect value is = 500, it will assign a number between -500 & 500 and it will assign to an axis (X,Y,Z)
+    float RandomVelocity() => Random.Range(-airSpinEffect, airSpinEffect); // Random Velocity for all axis
 }
